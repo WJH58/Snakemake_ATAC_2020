c7001b9fa9a8068dc87d0396dc8690cbec5c07dd
WJH58 <JWang@lumc.nl>
2020-11-13 16:12:04 +0100

    add scripts necessary for shark

---
+++ ATAC-seq_pipeline/slurm_minisnakemake.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#SBATCH  --job-name=mini_snakemake
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user j.chouaref@lumc.nl
+#SBATCH -t 48:00:00
+#SBATCH --mem=60000
+
+
+echo Start time : `date`
+snakemake -p \
+        --snakefile Snakefile \
+        --latency-wait 60 \
+        --wait-for-files \
+        --rerun-incomplete \
+        --use-conda \
+        --cluster "sbatch --parsable --partition=all --mem=60g --ntasks=1 --cpus-per-task=8 --time=60:00:00 --hint=multithread" \
+        --cluster-status "./slurm-cluster-status.py" \
+       	--jobs 30
+
+
+echo End time : `date`
+
